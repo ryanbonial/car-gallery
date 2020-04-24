30372567f1ff1f5793f894ac228473c65603ace8
Ryan Bonial <ryanbonial@gmail.com>
2020-04-24 14:24:03 -0600

    Add project files.

---
+++ CarGalleryApp/Controllers/CarsController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CarGalleryApp.Models;
+using CarGalleryApp.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarGalleryApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        private readonly CarService carService;
+
+        public CarsController(CarService carService) 
+        {
+            this.carService = carService;
+        }
+
+        // GET: api/Cars
+        [HttpGet]
+        public List<Car> Get()
+        {
+            return carService.Get();            
+        }
+
+        // GET: api/Cars/5
+        [HttpGet("{id}", Name = "Get")]
+        public Car Get(string id)
+        {
+            return carService.Get(id);
+        }
+
+        // POST: api/Cars
+        [HttpPost]
+        public void Post([FromBody] Car car)
+        {
+            carService.Create(car);
+        }
+
+        // PUT: api/Cars/5
+        [HttpPut("{id}")]
+        public void Put(string id, [FromBody] Car car)
+        {
+            carService.Update(id, car);
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(string id)
+        {
+            carService.Remove(id);
+        }
+    }
+}
---
+++ CarGalleryApp/CustomAttributes/YearRangeAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarGalleryApp.CustomAttributes
+{
+    public class YearRangeAttribute : RangeAttribute
+    {
+        public YearRangeAttribute() : base(typeof(int), DateTime.Now.AddYears(-50).Year.ToString(), DateTime.Now.AddYears(1).Year.ToString())
+        { }
+    }
+}
